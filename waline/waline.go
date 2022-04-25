8c86e81479304e2bad821cc844cb840f1f5e7670
qwqcode <qwqcode@gmail.com>
2022-04-25 15:38:10 +0800

    fix(waline): Replace vemoji class attr to atk-emoticon.
    Signed-off-by: qwqcode <qwqcode@gmail.com>

--- waline/waline.go
+++ waline/waline.go
@@ -31,8 +31,13 @@ func WalineToArtrans(wComments []Comment) []lib.Artran {
 		content := strings.TrimSpace(wC.Comment)
 
 		// 移除 "[@USERNAME](#rid): "
-		replyAtMatcher := regexp.MustCompile(`^\[@(.*?)\]\(#[a-zA-Z0-9]+?\)(: )?`)
+		replyAtMatcher := regexp.MustCompile(`^\[@(.*?)\]\(#[a-zA-Z0-9]+?\)(: | , )?`)
+		replyAtMatcher2 := regexp.MustCompile(`<a\s+(?:[^>]*?\s+)?href="#([a-zA-Z0-9]+)?"[^>]*>@(.*?)<\/a>(: | , )?`)
 		content = replyAtMatcher.ReplaceAllString(content, "")
+		content = replyAtMatcher2.ReplaceAllString(content, "")
+
+		vEmojiMatcher := regexp.MustCompile(`(<img[^>]*)(class=['"]vemoji['"])`)
+		content = vEmojiMatcher.ReplaceAllString(content, "${1}atk-emoticon")
 
 		artran := lib.Artran{
 			ID:        lib.ToString(wC.ID),
